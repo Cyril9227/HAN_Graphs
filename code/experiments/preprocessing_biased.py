bd98119835c126fa1f89f3bcda99dc6188293f5e
VinJac <vincent.jacob@edu.ece.fr>
2019-04-04 17:45:39 +0200

    Script de preprocessing indépendant pour toutes les targets, et ajout du dossier target_3

--- code/experiments/preprocessing_biased.py
+++ code/experiments/preprocessing_biased.py
@@ -25,26 +25,24 @@ def pad_docs(docs, sent_max, l_max):
                 s += [pad_vec_idx]*(l_max-len(s))
     return docs
 # = = = = = = = = = = = = = = =
-
+    
 pad_vec_idx = 1685894 # 0-based index of the last row of the embedding matrix (for zero-padding)
 
 # = = = = = = = = = = = = = = =
-### Parameters and paths
-sent_max = 160
-
-l_max = 11
-l_i = (8, 11) # interval for random sentences length
-
-r = 8         # nb of sentences by word
-p = 2         # return hyperparam
-q = 2       # in-out hyperparam
-
+### Paths
 path_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('').replace('\\', '/'))))
 path_to_data = path_root + '/data/'
 # = = = = = = = = = = = = = = =
 
-def main():
-
+def create_documents(target, sent_max, word_interval, word_rate, p, q):
+    '''
+    target: 0 to 3: number of target for which we want to use the embedded docs
+    sent_max: maximum number of sentences in produced documents
+    word_interval: (min, max) (included), random nb of words by sentence
+    word_rate (r): number of sentences that start from each word/node
+    p: return parameter (controls how often we stay at the same node within a walk)
+    q: in-out parameter (q << 1 => outward walks, DFS exploration, q >> 1 => inward walk, BFS exploration)
+    '''
     start_time = time() 
 
     edgelists = os.listdir(path_to_data + 'edge_lists/')
@@ -55,7 +53,7 @@ def main():
         g = Graph(nx.read_edgelist(path_to_data + 'edge_lists/' + edgelist), p, q) 
         g.preprocess_transition_probs()
         
-        doc = g.simulate_walks(r, l_i)
+        doc = g.simulate_walks(word_rate, word_interval)
         
         # create the pseudo-document representation of the graph
         docs.append(doc)
@@ -64,22 +62,17 @@ def main():
         if idx % 15000 == 0:
             print(idx)
 
-    print('documents generated')
+    print('documents generated for target', target)
     
     # truncation-padding at the document level, i.e., adding or removing entire 'sentences'
-    docs = pad_docs(docs, sent_max, l_max)
+    docs = pad_docs(docs, sent_max, word_interval[1])
     
     # docs = [d+[[pad_vec_idx]*(walk_length+1)]*(max_doc_size-len(d)) if len(d)<max_doc_size else d[:max_doc_size] for d in docs] 
 
     docs = np.array(docs).astype('int')
     print('document array shape:', docs.shape)
 
-    np.save(path_to_data + 'documents.npy', docs, allow_pickle=False)
+    np.save(path_to_data + 'documents_' + str(target) + '.npy', docs, allow_pickle=False)
 
-    print('documents saved')
+    print('documents saved for target', target)
     print('everything done in', str(round(time() - start_time, 2)) + 's')
-
-# = = = = = = = = = = = = = = =
-
-if __name__ == '__main__':
-    main()
