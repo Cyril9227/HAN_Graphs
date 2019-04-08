16bc1e7b2375aac54b3310f282db417b16c59ee0
VinJac <vincent.jacob@edu.ece.fr>
2019-04-08 19:16:55 +0200

    Possibilité de changer de distribution aléatoire pour le preprocessing + renommage

--- code/experiments/preprocessing_biased.py
+++ code/experiments/preprocessing_biased.py
@@ -20,9 +20,8 @@ def pad_docs(docs, sent_max, l_max):
     # sentence padding
     docs = [d+[[pad_vec_idx]*(l_max)]*(sent_max-len(d)) if len(d)<sent_max else d[:sent_max] for d in docs]
     for d in docs:
-        for s in d:
-            if len(s) < l_max:
-                s += [pad_vec_idx]*(l_max-len(s))
+        for i in range(len(d)):
+            d[i] = d[i] + [pad_vec_idx]*(l_max-len(d[i])) if len(d[i]) < l_max else d[i][:l_max]
     return docs
 # = = = = = = = = = = = = = = =
     
@@ -34,15 +33,18 @@ path_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('').
 path_to_data = path_root + '/data/'
 # = = = = = = = = = = = = = = =
 
-def create_documents(target, sent_max, word_interval, word_rate, p, q):
+def create_documents(target, sent_max, sent_l_law, sent_l_stats, word_rate, p, q):
     '''
     target: 0 to 3: number of target for which we want to use the embedded docs
     sent_max: maximum number of sentences in produced documents
-    word_interval: (min, max) (included), random nb of words by sentence
+    sent_l_law: 'uniform' or 'normal' => the distribution to follow when generating random sentence length
+    sent_l_stats: the interval of lengths if uniform, mean and std if normal distribution
     word_rate (r): number of sentences that start from each word/node
     p: return parameter (controls how often we stay at the same node within a walk)
     q: in-out parameter (q << 1 => outward walks, DFS exploration, q >> 1 => inward walk, BFS exploration)
     '''
+    if sent_l_law != 'uniform' and sent_l_law != 'normal':
+        return
     start_time = time() 
 
     edgelists = os.listdir(path_to_data + 'edge_lists/')
@@ -53,7 +55,7 @@ def create_documents(target, sent_max, word_interval, word_rate, p, q):
         g = Graph(nx.read_edgelist(path_to_data + 'edge_lists/' + edgelist), p, q) 
         g.preprocess_transition_probs()
         
-        doc = g.simulate_walks(word_rate, word_interval)
+        doc = g.simulate_walks(word_rate, sent_l_law, sent_l_stats)
         
         # create the pseudo-document representation of the graph
         docs.append(doc)
@@ -65,9 +67,8 @@ def create_documents(target, sent_max, word_interval, word_rate, p, q):
     print('documents generated for target', target)
     
     # truncation-padding at the document level, i.e., adding or removing entire 'sentences'
-    docs = pad_docs(docs, sent_max, word_interval[1])
-    
-    # docs = [d+[[pad_vec_idx]*(walk_length+1)]*(max_doc_size-len(d)) if len(d)<max_doc_size else d[:max_doc_size] for d in docs] 
+    word_max = sent_l_stats[1] if sent_l_law == 'uniform' else int(sent_l_stats[0] + 1.5 * sent_l_stats[1])
+    docs = pad_docs(docs, sent_max, word_max)
 
     docs = np.array(docs).astype('int')
     print('document array shape:', docs.shape)
